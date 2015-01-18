1184b40a075323e1aeed92d037a8292ff3b562ef
uuspider <wtking@gmail.com>
2015-01-18 12:18:54 +0800

    update javascript

--- javascripts/slide.js
+++ javascripts/slide.js
@@ -1,3 +1,3 @@
 var m=35;
 var n=Math.floor(Math.random()*m+1)
-document.write("<img src='./images/slide/"+"img"+n+".png'>");
+document.write("<img src='./images/slide/"+"img"+n+".png' alt="episodes of uuspider\'s life" />");
