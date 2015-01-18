523ac6c97e800600670b84e379a8513f9d69df13
uuspider <wtking@gmail.com>
2015-01-18 12:35:27 +0800

    update javascript

--- javascripts/slide.js
+++ javascripts/slide.js
@@ -1,3 +1,3 @@
 var m=35;
 var n=Math.floor(Math.random()*m+1)
-document.write("<img src='./images/slide/"+"img"+n+".png' alt="Episode of uuspider" />");
+document.write("<img src='./images/slide/"+"img"+n+".png' alt='Episode of uuspider' title='Episode of uuspider' />");
