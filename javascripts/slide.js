918d0c76a8f565198e695dc045c1dd55e379d6b7
uuspider <wtking@gmail.com>
2020-06-13 22:43:40 +0800

    add: slide img

--- javascripts/slide.js
+++ javascripts/slide.js
@@ -1,3 +1,3 @@
-var m=48;
+var m=57;
 var n=Math.floor(Math.random()*m+1)
 document.write("<a href='/2019/06/02/episodeindex.html' target='_blank'><img src='./images/slide/"+"img"+n+".png' alt='Episode of uuspider' title='Episode of uuspider' /></a>");
