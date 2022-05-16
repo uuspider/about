ce93800ce3f299d7b069a6e2862919276d262968
uuspider <wtking@gmail.com>
2022-05-17 06:01:13 +0800

    update 404.html

--- javascripts/slide.js
+++ javascripts/slide.js
@@ -1,3 +1,3 @@
 var m=58;
 var n=Math.floor(Math.random()*m+1)
-document.write("<a href='/2019/06/02/episodeindex.html' target='_blank'><img src='./images/slide/"+"img"+n+".jpg' alt='Episode of uuspider' title='Episode of uuspider' /></a>");
+document.write("<a href='https://about.uuspider.com/2019/06/02/episodeindex.html' target='_blank'><img src='https://about.uuspider.com/images/slide/"+"img"+n+".jpg' alt='Episode of uuspider' title='Episode of uuspider' /></a>");
