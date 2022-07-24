c83751717e81fe8de045a722a4dc63395e49094a
uuspider <wtking@gmail.com>
2022-07-25 01:39:29 +0800

    update: del _site

--- _site/README.md
+++
@@ -1,7 +0,0 @@
-TODO
-
-1. selenium
-2. scrapy
-3. neural networks & deep learning
-4. matplotlib
-5. recommender system
--- _site/javascripts/slide.js
+++
@@ -1,3 +0,0 @@
-var m=48;
-var n=Math.floor(Math.random()*m+1)
-document.write("<img src='./images/slide/"+"img"+n+".png' alt='Episode of uuspider' title='Episode of uuspider' />");
